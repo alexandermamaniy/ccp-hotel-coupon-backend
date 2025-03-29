c6f247c09c64c32820aa85d23effebff6bd08437
Alexander Mamani Yucra <x23329823@student.ncirl.ie>
2025-03-29 10:46:34 +0000

    remove PDF converter

--- reports/urls.py
+++ reports/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import GenerateReportPDFView, ListReportAPIView, GenerateReportPDFCustomView, PdfToWordView
+from .views import GenerateReportPDFView, ListReportAPIView, GenerateReportPDFCustomView
 
 urlpatterns = [
     path('report/generate-pdf', GenerateReportPDFView.as_view(), name='generate-report-pdf'),
     path('report/me', ListReportAPIView.as_view(), name='report-pdf-me'),
     path('generate_pdf', GenerateReportPDFCustomView.as_view(), name='report-pdf'),
-    path('convert/', PdfToWordView.as_view(), name='pdf-to-word'),
 ]
